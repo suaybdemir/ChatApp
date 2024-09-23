c0f85ee0bb9846eeda6e71f8d0b2d5713527cec5
win10ryzen53500u8 <byausrimed@gmail.com>
2024-09-23 23:53:56 +0300

    micro-commit 1-Added fetching username to front side

--- ChatAppServer/ChatAppServer.WebAPI/obj/Debug/net8.0/ChatAppServer.WebAPI.AssemblyInfo.cs
+++ ChatAppServer/ChatAppServer.WebAPI/obj/Debug/net8.0/ChatAppServer.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c5ae17dd007acf56dbc4134a0e706752758a934")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c85819870906fda3a644ecae3d629ee7b86d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
